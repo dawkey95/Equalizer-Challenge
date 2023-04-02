e7fcc198683d753f8ba5b5d0e16b97d491a35a35
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-02 18:53:37 +1200

    Mobile version completed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", "sans-serif"],
+      },
+
+      colors: {
+        "sky-blue": "#66E2DC",
+        "outrageous-orange": "#FA7453",
+        rajah: "#FFB964",
+        snow: "#FCFAF9",
+        "eerie-black": "#191826",
+        background: "#F2F2F2",
+      },
+
+      screens: {
+        desktop: { max: "1440px" },
+        tablet: { max: "768px" },
+        mobile: { max: "375px" },
+      },
+    },
+  },
+  plugins: [],
+};
